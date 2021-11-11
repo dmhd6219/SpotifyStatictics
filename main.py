53c52861e1f4158ac3133d9b853bae35ea5828b9
dmhd <svyatoslavsvyatkin@yandex.ru>
2021-11-11 18:23:18 +0300

    added profile template

--- main.py
+++ main.py
@@ -19,43 +19,5 @@ flask_session.Session(app)
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-
-@app.route('/')
-@spotify_login_required
-def index(spotify: Spotify):
-    account = spotify.me()
-
-    pprint(account)
-    return render_template('index.html', data=account)
-
-
-@app.route('/stats/tracks/<term>')
-@spotify_login_required
-def tracks_stats(spotify: Spotify, term):
-    if term not in ('short', 'medium', 'long'):
-        abort(404)
-
-    tracks = []
-    for i in range(5):
-        for track in spotify.current_user_top_tracks(limit=20, offset=20 * i, time_range=term + '_term')['items']:
-            tracks.append(track)
-
-    return render_template('tracks_test.html', data=tracks, term=term)
-
-
-@app.route('/stats/artists/<term>')
-@spotify_login_required
-def artists_stats(spotify: Spotify, term):
-    if term not in ('short', 'medium', 'long'):
-        abort(404)
-
-    artists = []
-    for i in range(5):
-        for artist in spotify.current_user_top_artists(limit=20, offset=20 * i, time_range=term + '_term')['items']:
-            artists.append(artist)
-
-    return render_template('artists.html', data=artists, term=term)
-
-
 if __name__ == '__main__':
     app.run(threaded=True, port=8080)
